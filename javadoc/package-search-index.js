60eb023ebb87137561fb463625c46568453a023e
Pierre-Emmanuel REBOURS <pe.rebours@etu.emse.fr>
2022-01-10 19:36:17 +0100

    add javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.pokedex"},{"l":"com.example.pokedex.controllers"},{"l":"com.example.pokedex.models"},{"l":"com.example.pokedex.services"},{"l":"com.example.pokedex.views"}]
